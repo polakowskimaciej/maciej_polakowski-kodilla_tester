1c93e89bf3e7422ec4849ce490a937c6a1af35a6
Maciej Polakowski <polakowski.maciej@gmail.com>
2022-06-12 21:40:33 +0200

    VariablesClass

---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main (String [] args) {
+        String myName = "Maciej";
+        int myAge = 30;
+        double height = 1.82;
+        char carClasss = 'A';
+        boolean isGoodProgrammer = true;
+        String example = "Hello from first class!";
+        System.out.println(example);
+    }
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,29 @@
+public class MainExtended {
+    public static void main (String [] args) {
+        int NumberOfCats = 3;
+        String Cat1 = "Igorek";
+        String Cat2 = "Olunia";
+        String Cat3 = "Mafia";
+        double Cat1Weight = 4.80;
+        double Cat2Weight = 3.90;
+        double Cat3Weight = 4.20;
+        boolean isHungry = true;
+        char colorClass1 = 'B';
+        char colorClass2 = 'T';
+        boolean isSick = false;
+        System.out.println(NumberOfCats);
+        System.out.println(Cat1);
+        System.out.println(colorClass1);
+        System.out.println(Cat1Weight);
+        System.out.println(isHungry);
+        System.out.println(Cat2);
+        System.out.println(colorClass1);
+        System.out.println(Cat2Weight);
+        System.out.println(isHungry);
+        System.out.println(Cat3);
+        System.out.println(colorClass2);
+        System.out.println(Cat3Weight);
+        System.out.println(isHungry);
+        System.out.println(isSick);
+    }
+}
