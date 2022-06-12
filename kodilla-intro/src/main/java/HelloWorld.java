edae21a8fdf415785175c399fae936e420848161
Maciej Polakowski <polakowski.maciej@gmail.com>
2022-06-12 20:41:43 +0200

    First Class and Hello World

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {                           // [1]
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello World!");
+        System.out.println("Maciej");
+        System.out.println(30);
+        System.out.println('P');// [3]
+    }
+}
