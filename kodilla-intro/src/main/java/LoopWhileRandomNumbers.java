67df0b7ac03421dbf57a049a849987ce6e980f6e
Maciej Polakowski <polakowski.maciej@gmail.com>
2022-06-21 18:46:19 +0200

    Korekta na spotkaniu 2106

--- kodilla-intro/src/main/java/LoopWhileRandomNumbers.java
+++ kodilla-intro/src/main/java/LoopWhileRandomNumbers.java
@@ -1,21 +1,15 @@
 import java.util.Random;
 
 public class LoopWhileRandomNumbers {
-    getCountOfRandomNumber()
-
-}
-
-    public static void main(String[] args) {
-        public int getCountOfRandomNumber ( int max){
-            Random random = new Random();
-            int result = 0;
-            int sum = 0;
-            while (sum < max) {
-                int temp = random.nextInt(10);
-                sum = sum + temp;
-                result++;
-            }
-            return (result);
+    public int getCountOfRandomNumber(int max) {
+        Random random = new Random();
+        int result = 0;
+        int sum = 0;
+        while (sum < max) {
+            int temp = random.nextInt(10);
+            sum = sum + temp;
+            result++;
         }
+        return (result);
     }
 }
---
+++ kodilla-intro/src/main/java/MainLoop.java
@@ -0,0 +1,7 @@
+public class MainLoop {
+    public static void main(String[] args) {
+        LoopWhileRandomNumbers abc = new LoopWhileRandomNumbers();
+        int result = abc.getCountOfRandomNumber(15);
+        System.out.println(result);
+    }
+}
