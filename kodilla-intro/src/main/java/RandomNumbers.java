1c90c66e6f90732c1d5d80941456398384b8d2ec
Maciej Polakowski <polakowski.maciej@gmail.com>
2022-06-26 19:00:42 +0200

    2.5 Users 2.5 wrong RandomNumbers 2.6 Application

---
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+public class RandomNumbers {
+    public int getCountOfRandomNumberuntill500(int max) {
+        Random random = new Random();
+        int result = 0;
+        int sum = 0;
+        while (sum < max) {
+            int temp = random.nextInt(30);
+            sum = sum + temp;
+            result++;
+        }
+        return (result);
+    }
+    private int getSmallest(){
+        int counted = getCountOfRandomNumberuntill500(500);
+        for (int i = 0; i < counted; i++) {
+            int min = counted(0);
+            if(counted[i]<min){
+                min = counted[i];
+            }
+            return min;
+    }
+    private int getBiggest(){
+        int counted = getCountOfRandomNumberuntill500(500);
+        for (int i = 0; i < counted; i++) {
+            int max = counted[0];
+            if(counted[i]>max){
+                max = counted[i];
+            }
+            return max;
+        }
+    public static void main(String[] args) {
+        int result = getCountOfRandomNumberuntill500(500);
+        System.out.println(result);
+    }
+}
--- kodilla-intro/src/main/java/User.java
+++ kodilla-intro/src/main/java/User.java
@@ -32,12 +32,14 @@ public class User {
 //obliczenie średniej
         int result = 0;
         for (int i = 0; i < users.length; i++) { //dla każedej wartości i od zera i mniejszej od długości tablicy, dodajemy jeden
-            result = result + users[i].getAge();
+            result = result + users[i].getAge(); //w końcu ogarnąłem jak odnieść się do getAge() dla każdego użytkownika w każdej iteracji
         }
         int average = result / users.length;
+        //wyświetlanie użytkowników młodszych niż średnia
+        System.out.println("Users younger than average:");
         for (int i = 0; i < users.length; i++) {
             if (users[i].getAge() < average) {
-                System.out.println(users[i].getAge());
+                System.out.println(users[i].getName() + " age: " + users[i].getAge());
             }
 
         }
