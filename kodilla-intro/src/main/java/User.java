4b740fe6addaf1cc03277fd9284f6c78245c2c5f
Maciej Polakowski <polakowski.maciej@gmail.com>
2022-06-27 18:46:00 +0200

    User podzielony na dwie klasy: User i UseMain

--- kodilla-intro/src/main/java/User.java
+++ kodilla-intro/src/main/java/User.java
@@ -21,28 +21,4 @@ public class User {
     }
 
     //metoda main do dodania obiektów,tablicy, wyświetleń
-    public static void main(String[] args) {
-        User maciej = new User(30, "Maciej");
-        User michal = new User(15, "Michał");
-        User kasia = new User(20, "Kasia");
-        User grzegorz = new User(40, "Grzegorz");
-        User halina = new User(50, "Halina");
-        User[] users = {maciej, michal, kasia, grzegorz, halina};
-        //tablica obiektów,
-//obliczenie średniej
-        int result = 0;
-        for (int i = 0; i < users.length; i++) { //dla każedej wartości i od zera i mniejszej od długości tablicy, dodajemy jeden
-            result = result + users[i].getAge(); //w końcu ogarnąłem jak odnieść się do getAge() dla każdego użytkownika w każdej iteracji
-        }
-        int average = result / users.length;
-        //wyświetlanie użytkowników młodszych niż średnia
-        System.out.println("Users younger than average:");
-        for (int i = 0; i < users.length; i++) {
-            if (users[i].getAge() < average) {
-                System.out.println(users[i].getName() + " age: " + users[i].getAge());
-            }
-
-        }
-
-    }
 }
