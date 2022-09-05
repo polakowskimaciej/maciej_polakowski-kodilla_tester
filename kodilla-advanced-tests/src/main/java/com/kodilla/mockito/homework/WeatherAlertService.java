79968d288996edf726c79405aea67dc1fd673c5d
Maciej Polakowski <polakowski.maciej@gmail.com>
2022-09-05 12:25:56 +0200

    Weather Alert Service 1.0

--- kodilla-advanced-tests/src/main/java/com/kodilla/mockito/homework/WeatherAlertService.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/mockito/homework/WeatherAlertService.java
@@ -3,27 +3,36 @@ package com.kodilla.mockito.homework;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class WeatherAlertService {
     private HashMap<Person, List<Location>> personLocationHashMap = new HashMap<>();
 
     public void addPerson(Person person, Location location) {
-        List<Location> locationsList = personLocationHashMap.getOrDefault(person,new ArrayList<>());
+        List<Location> locationsList = personLocationHashMap.getOrDefault(person, new ArrayList<>());
         locationsList.add(location);
         this.personLocationHashMap.put(person, locationsList);
     }
-    public void addLocation(Location location) {}
 
     public void removePersonFromLocation(Person person, Location location) {
-        List<Location> locationsList = personLocationHashMap.getOrDefault(person,new ArrayList<>());
+        List<Location> locationsList = personLocationHashMap.getOrDefault(person, new ArrayList<>());
         locationsList.remove(location);
         this.personLocationHashMap.put(person, locationsList);
     }
 
-    public void removePersonFromDataBase(Person person){}
+    public void removePersonFromDataBase(Person person) {
+        this.personLocationHashMap.remove(person);
+    }
 
-    public void removeLocation(Location location){}
+    public void removeLocation(Location location) {
+        //usunąć lokalizację dla każdego key person który ją zawiera
+        for (Map.Entry<Person, List<Location>> personListEntry :
+                personLocationHashMap.entrySet()) {
+            removePersonFromLocation(personListEntry.getKey(), location);
+        }
+    }
+
     //do osób obserwujących daną lokalizację – powiadomienie np. o zagrożeniu gradem.
     public void sendAlertToLocation(Alert alert, Location location) {
         List<Person> peoplefromlocation = personLocationHashMap.entrySet()
@@ -33,7 +42,11 @@ public class WeatherAlertService {
                 .collect(Collectors.toList());
         peoplefromlocation.forEach(person -> person.recieve(alert));
     }
+
     //do wszystkich osób – informacje np. o zmieniającym się regulaminie,
-    public void sendAlertToAll(Alert alert) {}
+    public void sendAlertToAll(Alert alert) {
+        List<Person> everyone = personLocationHashMap.entrySet().stream().map(personListEntry -> personListEntry.getKey()).collect(Collectors.toList());
+        everyone.forEach(person -> person.recieve(alert));
+    }
 
 }
--- kodilla-advanced-tests/src/test/java/com/kodilla/mockito/homework/WeatherAlertServiceTest.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/mockito/homework/WeatherAlertServiceTest.java
@@ -13,6 +13,7 @@ class WeatherAlertServiceTest {
 
     WeatherAlertService weatherAlertService = new WeatherAlertService();
     Person person = Mockito.mock(Person.class);
+    Person person2 = Mockito.mock(Person.class);
     Location location = Mockito.mock(Location.class);
     Location location2 = Mockito.mock(Location.class);
     Alert alert = Mockito.mock(Alert.class);
@@ -24,46 +25,60 @@ class WeatherAlertServiceTest {
         //given - osoba dodana do lokalizacji
 
 
-        weatherAlertService.addPerson(person,location);
+        weatherAlertService.addPerson(person, location);
         //when - wysle powiadomienie
 
-        weatherAlertService.sendAlertToLocation(alert,location);
+        weatherAlertService.sendAlertToLocation(alert, location);
         //than - osoba otrzyma powiadomienie
         Mockito.verify(person).recieve(alert);
     }
 
     @Test
-    void addLocation() {
-    }
-
-    @Test
     void removePersonFromLocation() {
         //Można wycofać subskrypcję z danej lokalizacji
-        weatherAlertService.addPerson(person,location);
-        weatherAlertService.addPerson(person,location2);
+        weatherAlertService.addPerson(person, location);
+        weatherAlertService.addPerson(person, location2);
         //given - dwie lokalizacje
         //when - usuwam lok. wysyłam alert do dwóch lok
-        weatherAlertService.removePersonFromLocation(person,location);
-        weatherAlertService.sendAlertToLocation(alert,location);
-        weatherAlertService.sendAlertToLocation(alert,location2);
+        weatherAlertService.removePersonFromLocation(person, location);
+        weatherAlertService.sendAlertToLocation(alert, location);
+        weatherAlertService.sendAlertToLocation(alert, location2);
         //than - osoba dostaje jeden alert
         Mockito.verify(person, Mockito.atMostOnce()).recieve(alert);
     }
 
     @Test
     void removePersonFromDataBase() {
+        //given - person added
+        weatherAlertService.addPerson(person, location);
+        //when - usuwam osobę
+        weatherAlertService.removePersonFromDataBase(person);
+        weatherAlertService.sendAlertToLocation(alert, location);
+        //than - nie ma osoby w mapie, nie otrzymuje alertu
+        Mockito.verify(person, Mockito.never()).recieve(alert);
     }
 
     @Test
     void removeLocation() {
+        //given - added location
+        weatherAlertService.addPerson(person, location);
+        weatherAlertService.addPerson(person2, location);
+        //when - removeLocation
+        weatherAlertService.removeLocation(location);
+        weatherAlertService.sendAlertToLocation(alert, location);
+        //than - nikt z osób z lokalizacji nie dostaje alertu
+        Mockito.verify(person, Mockito.never()).recieve(alert);
+        Mockito.verify(person2, Mockito.never()).recieve(alert);
     }
 
-    @Test
-    void sendAlertToLocation() {
-    }
 
     @Test
     void sendAlertToAll() {
+        weatherAlertService.addPerson(person, location);
+        weatherAlertService.addPerson(person2, location2);
+        weatherAlertService.sendAlertToAll(alert);
+        Mockito.verify(person).recieve(alert);
+        Mockito.verify(person2).recieve(alert);
     }
 
 
