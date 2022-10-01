c6577971dc92bc948e61b079910cce2ef88c1728
Maciej Polakowski <polakowski.maciej@gmail.com>
2022-10-01 10:07:15 +0200

    Zadanie: Rozbudowa API

---
+++ kodilla-spring-basic/src/main/java/SpringBasicRunner.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicRunner {
+
+    public static void main(String[] args) {
+
+    }
+}
