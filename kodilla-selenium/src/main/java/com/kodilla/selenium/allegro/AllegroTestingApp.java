1eb63a8ea5d450bc6988e0eb042ca7c95b261665
Maciej Polakowski <polakowski.maciej@gmail.com>
2022-10-03 11:44:05 +0200

    Zadanie: Cwiczymy selektory CSS

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -4,8 +4,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public class AllegroTestingApp {
     public static void main(String[] args) {
@@ -17,13 +20,19 @@ public class AllegroTestingApp {
         //alert.accept();
 
 
-        driver.findElement(By.xpath("//*[@id=\"opbox-gdpr-consents-modal\"]/div/div[2]/div/div[2]/button[1]")).click();
-        WebElement categoryCombo = driver.findElement(By.xpath("//select[@aria-label=\"Kategoria i opcje wyszukiwania\"]"));
+        driver.findElement(By.cssSelector("button[data-role*=accept-consent]")).click();
+        WebElement categoryCombo = driver.findElement(By.cssSelector("select[data-role*=filters-dropdown-toggle]"));
         Select categorySelect = new Select(categoryCombo);
         categorySelect.selectByVisibleText("Elektronika");
-        WebElement inpuField = driver.findElement(By.xpath("//input[@data-role=\"search-input\"]"));
+        WebElement inpuField = driver.findElement(By.cssSelector("input[data-role*=search-input]"));
         inpuField.sendKeys("Mavic mini");
         inpuField.submit();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("section > article")));
+        List<WebElement> element = driver.findElements(By.cssSelector("section > article"));
+        for (WebElement element1 : element) {
+            System.out.println(element1.getText());
+            System.out.println("/----------------------/");
+        }
     }
 
 }
